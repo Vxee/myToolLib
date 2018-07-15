3f0ab7393dfbe2cf7fac63081d491b18cdf58f72
Vxee <15258691200@163.com>
2018-07-15 14:17:32 +0800

    添加函数节流防抖，深浅拷贝

--- index.js
+++ index.js
@@ -16,7 +16,6 @@
 
     var _ = {};
 
-
     _.swapArray = function(arr,i,j){
         var temp = arr[i];
         arr[i] = arr[j];
@@ -67,6 +66,106 @@
         }
         return arr;
     }
+
+    // 快速排序
+    _.quickSort = function(arr, left, right){
+        left = typeof left !== 'number' ? 0 : left;
+        right = typeof right !== 'number' ? arr.length-1 : right;
+        if(left < right){
+            position = getPos(arr, left, right);
+            this.quickSort(arr, left, position - 1);
+            this.quickSort(arr, position + 1, right);
+        }
+        return arr;
+    }
     
+    function getPos(arr,left,right){
+        var pivot = left+1,
+            current = arr[left];
+        for(var i = pivot; i <= right; i++){
+            if(current > arr[i]){
+                _.swapArray(arr, pivot, i);
+                pivot++;
+            }
+        }
+        _.swapArray(arr,pivot-1,left);
+        return pivot-1;
+    }
+
+    // 节流
+    _.throttle = function(fn, delay){
+        var last,
+            timer,
+            interval = delay || 200; 
+        return function(){
+            var now = new Date();
+                th = this,
+                args = arguments;
+            
+            if(last && now - last < interval){
+                clearTimeout(timer);
+                timer = setTimeout(function(){
+                    last = now;
+                    fn.apply(th,args);
+                },interval);
+            }else{
+                last = now;
+                fn.apply(th,args);
+            }
+        }
+    }
+
+    // 函数防抖
+    _.debounce = function(fn, delay){
+        var last,
+            timer,
+            interval = delay || 200;
+        return function(){
+            var th = this,
+                args = arguments;
+            if(timer){
+                clearTimeout(timer);
+            }
+            timer = setTimeout(function(){
+                timer = null;
+                fn.apply(th,args);
+            },interval);
+        }
+    }
+
+    // 浅拷贝
+    _.shallowCopy = function(obj){
+        if(typeof obj === null || typeof obj !== 'object') return;
+        var result;
+        if(Array.isArray(obj)){  // array
+            result = obj;
+        }else{  // object
+            result = {};
+            for(var key in obj){
+                if(obj.hasOwnProperty(key)) result[key] = obj[key];
+            }
+        }
+        return obj;
+    }
+
+    // 深拷贝
+    _.deepCopy = function(obj1,obj2){
+        if(JSON.parse){ // 这种方法的缺陷是会破坏原型链 并且无法拷贝属性值为function的属性
+            obj2 = JSON.parse(JSON.stringify(obj1));
+            return obj2;
+        }else{
+            obj2 = obj2 || {};
+            for(var key in obj){
+                if(typeof obj[key] === 'object'){
+                    obj2[key] = (obj[key].constructor === 'array') ? [] : {};
+                    this.deepCopy(obj2[key],obj1[key]);
+                }else{
+                    obj2[key] = obj1[key];
+                }
+            }
+        }
+        return obj2;
+    }
+
     return _;
 })
